5bd65058d4607c760448acd7ec3cf9ad1c103f67
WaintingLI <waitinglees@gmail.com>
2025-03-05 17:44:48 +0800

    修正隨機亂數的錯誤(縮排錯誤)

--- 小功能測試/randon_test.py
+++ 小功能測試/randon_test.py
@@ -6,11 +6,11 @@ import random
 
 
 if __name__ == "__main__":
-        #使用亂數來創造名字
-        string_nuber = 8
-        print_string = ""
-        get_str_list = random.sample('1234567890zyxwvutsrqponmlkjihgfedcba',string_nuber)
-        for number in range(string_nuber):
-                print_string = print_string + get_str_list[number]
-                return_pvc_name = "app_pvc_name"+"-"+print_string
-        print(return_pvc_name)
+    #使用亂數來創造名字
+    string_nuber = 8
+    print_string = ""
+    get_str_list = random.sample('1234567890zyxwvutsrqponmlkjihgfedcba',string_nuber)
+    for number in range(string_nuber):
+            print_string = print_string + get_str_list[number]
+    return_pvc_name = "app_pvc_name"+"-"+print_string
+    print(return_pvc_name)
